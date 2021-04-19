4f5a90e3d74acfa8d659eaa2d98d2991a887c23b
Turkyden <wj871287@gmail.com>
2021-04-19 18:08:59 +0800

    style: 💄 optiming the style of landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,25 +7,14 @@ export default function IndexPage() {
       <header className="text-gray-400 bg-gray-900 body-font">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              className="w-10 h-10 text-white p-2 bg-blue-500 rounded-full"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-            </svg>
+            <span className="text-2xl">☄️ </span>
             <span className="ml-3 text-xl">Digital Go</span>
           </a>
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-            <a className="mr-5 hover:text-white">首页</a>
-            <a className="mr-5 hover:text-white">功能亮点</a>
-            <a className="mr-5 hover:text-white">产品定价</a>
-            <a className="mr-5 hover:text-white">产品学院</a>
+            <span className="mr-5 hover:text-white">首页</span>
+            <span className="mr-5 hover:text-white">功能亮点</span>
+            <span className="mr-5 hover:text-white">产品定价</span>
+            <span className="mr-5 hover:text-white">产品学院</span>
           </nav>
           <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
             联系我们
@@ -162,7 +151,7 @@ export default function IndexPage() {
                   />
                 </div>
                 <button className="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded">
-                  Button
+                  E-Mail
                 </button>
               </div>
               <p className="text-gray-500 text-sm mt-2 md:text-left text-center">
