f9f42ad3dc6ff44883d423d0712399026dbd4568
Turkyden <wj871287@gmail.com>
2021-05-05 17:32:13 +0800

    feat: 🎸 fixed the folder name

--- .umirc.ts
+++ .umirc.ts
@@ -8,13 +8,13 @@ export default defineConfig({
     type: 'none',
   },
   routes: [
-    { exact: true, path: '/', component: '@/pages/Index.tsx' },
-    { exact: true, path: '/editor', component: '@/pages/Editor.tsx' },
-    { exact: true, path: '/login', component: '@/pages/Login.tsx' },
+    { exact: true, path: '/', component: '@/pages/index' },
+    { exact: true, path: '/editor', component: '@/pages/editor' },
+    { exact: true, path: '/login', component: '@/pages/login' },
     {
       exact: false,
       path: '/dashboard',
-      component: '@/pages/Dashboard/index.tsx',
+      component: '@/pages/dashboard',
       routes: [],
     },
   ],
