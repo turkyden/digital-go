7d545c3380b03652bb9b01cbf7637b6a0f346725
Turkyden <wj871287@gmail.com>
2021-05-05 17:19:17 +0800

    fixed: config file

--- .umirc.ts
+++ .umirc.ts
@@ -8,13 +8,13 @@ export default defineConfig({
     type: 'none',
   },
   routes: [
-    { exact: true, path: '/', component: '@/pages/Index' },
-    { exact: true, path: '/editor', component: '@/pages/Editor' },
-    { exact: true, path: '/login', component: '@/pages/Login' },
+    { exact: true, path: '/', component: '@/pages/Index.tsx' },
+    { exact: true, path: '/editor', component: '@/pages/Editor.tsx' },
+    { exact: true, path: '/login', component: '@/pages/Login.tsx' },
     {
       exact: false,
       path: '/dashboard',
-      component: '@/pages/Dashboard',
+      component: '@/pages/Dashboard/index.tsx',
       routes: [],
     },
   ],
